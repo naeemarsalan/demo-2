68693331e04e4461b151419fe686426918fb0a26
Arsalan Naeem <naeemarsalan@gmail.com>
2024-04-04 16:35:06 +0100

    Cleaned up

--- 1-demo/src/main/java/com/tucanoo/crm/pdfcreator/EmployeePDFCreator.java
+++ 1-demo/src/main/java/com/tucanoo/crm/pdfcreator/EmployeePDFCreator.java
@@ -1,9 +1,5 @@
 package com.tucanoo.crm.pdfcreator;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -60,36 +56,5 @@ public class EmployeePDFCreator {
              }
 
         }
-
-        // try {
-           
-            // PDPageContentStream contentStream = new PDPageContentStream(document, page);.
-            // contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            // contentStream.beginText();
-            // contentStream.newLineAtOffset(100, 700);
-            // contentStream.showText("Employee Details");
-            // contentStream.newLine();
-            // contentStream.showText("First Name: " + employee.get("firstName").getAsString());
-            // contentStream.newLine();
-            // contentStream.showText("Last Name: " + employee.get("lastName").getAsString());
-            // contentStream.newLine();
-            // contentStream.showText("Country: " + employee.get("country").getAsString());
-            // contentStream.newLine();
-            // contentStream.showText("Email Address: " + employee.get("emailAddress").getAsString());
-            // contentStream.newLine();
-            // contentStream.showText("Phone Number: " + employee.get("phoneNumber").getAsString());
-            // contentStream.newLine();
-            // contentStream.showText("City: " + employee.get("city").getAsString());
-            // contentStream.endText();
-            // contentStream.close();
-
-            // String fileName = employee.get("firstName").getAsString() + "_" + employee.get("lastName").getAsString() + ".pdf";
-            // document.save(fileName);
-            // document.close();
-
-            // System.out.println("PDF created successfully: " + fileName);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
     }
 }
