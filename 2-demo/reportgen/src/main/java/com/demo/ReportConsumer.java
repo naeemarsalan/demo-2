0ab62b2dc94964f7386746922eaa437e5448299f
Arsalan Naeem <naeemarsalan@gmail.com>
2024-05-12 13:08:47 +0100

    Parsed JSON

--- 2-demo/reportgen/src/main/java/com/demo/ReportConsumer.java
+++ 2-demo/reportgen/src/main/java/com/demo/ReportConsumer.java
@@ -1,15 +1,31 @@
 package com.demo;
 
+import java.io.IOException;
+
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.jboss.logging.Logger;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ReportConsumer {
 
     private final Logger logger = Logger.getLogger(ReportConsumer.class);
+    private final ObjectMapper objectMapper;
+
+    public ReportConsumer() {
+        objectMapper = new ObjectMapper();
+        // Ignore unrecognized properties
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     @Incoming("report")
-    public void receive(String customer) {
-        logger.infof("Customer %s", customer);
+    public void receive(String customerJson) {
+        try {
+            Customer customer = objectMapper.readValue(customerJson, Customer.class);
+            logger.infof("Received customer: %s", customer.getId());
+        } catch (IOException e) {
+            logger.error("Failed to parse customer JSON", e);
+        }
     }
 }
